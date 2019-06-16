250051d963c5a3ac40b48573915ce4a860e51aa7
Darwin Smith <darwinpsmith@gmail.com>
2019-06-16 12:11:10 -0400

    consolidates api calls into helper.js

---
+++ src/services/helper.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const KEY = process.env.REACT_APP_API_KEY;
+const URL =
+  "https://cors-anywhere.herokuapp.com/https://feeds.citibikenyc.com/stations/stations.json";
+
+const getStationData = async () => {
+  const resp = await axios(`${URL}`);
+  console.log(resp.data);
+  return resp.data.stationBeanList.filter(
+    station => station.statusValue === "In Service"
+  );
+};
+
+const formatAddress = async location => {
+  const resp = await axios(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&components=country:US&key=${KEY}`
+  );
+  return resp.data.results[0];
+};
+
+export { getStationData, formatAddress };
