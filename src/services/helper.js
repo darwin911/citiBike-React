42031be1c0bb43255cb0cd04f1b0f3dd71ac3f5d
Darwin Smith <darwin911@gmail.com>
2019-12-22 16:51:04 -0500

    reverts to initial query

--- src/services/helper.js
+++ src/services/helper.js
@@ -4,11 +4,11 @@ const KEY = process.env.REACT_APP_API_KEY;
 const URL =
   'https://cors-anywhere.herokuapp.com/https://feeds.citibikenyc.com/stations/stations.json';
 
-const getStationData = async max => {
+const getStationData = async () => {
   const resp = await axios(`${URL}`);
-  return resp.data.stationBeanList
-    .filter(station => station.statusValue === 'In Service')
-    .slice(0, max);
+  return resp.data.stationBeanList.filter(
+    station => station.statusValue === 'In Service'
+  );
 };
 
 const formatAddress = async location => {
