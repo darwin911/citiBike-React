1db2574687fe58fa1940e7213ddbf479ebb3f84b
Darwin Smith <darwin911@gmail.com>
2019-12-22 16:50:49 -0500

    change to PascalCase

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 const Header = () => {
   return (
     <header>
-      <h1 className="title">citi<span>bike</span></h1>
+      <h1 className='title'>
+        Citi<span>Bike</span>
+      </h1>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
