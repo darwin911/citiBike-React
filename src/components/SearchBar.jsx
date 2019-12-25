37fdc74a580b0671ff94a8cb9ec6f938723753ca
Darwin Smith <darwin911@gmail.com>
2019-12-25 10:31:53 -0500

    creates handler for input form

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,35 +1,38 @@
-import React from "react";
+import React from 'react';
 
 const SearchBar = ({ handleSubmit, handleChange, origin, destination }) => {
+  const handleClick = e => {
+    e.preventDefault();
+  };
+
   return (
-    <nav>
+    <nav className='search-bar-container'>
       <form
         onSubmit={e => {
           e.preventDefault();
           handleSubmit(origin, destination);
-        }}
-      >
+        }}>
         <input
-          className="origin-input"
-          type="text"
-          placeholder="Origin"
-          name="origin"
+          className='origin-input'
+          type='text'
+          placeholder='Origin'
+          name='origin'
           onChange={handleChange}
           value={origin.text}
           required
         />
 
         <input
-          className="destination-input"
-          type="text"
-          placeholder="Destination"
-          name="destination"
+          className='destination-input'
+          type='text'
+          placeholder='Destination'
+          name='destination'
           onChange={handleChange}
           value={destination.text}
           required
         />
 
-        <input className="submit" type="submit" value="Find Stations" />
+        <input className='submit' type='submit' value='Go!' />
       </form>
     </nav>
   );
