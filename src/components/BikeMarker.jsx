edc76f741d6cbfa138f54fc66f1b7cff7c47baeb
Darwin Smith <darwin911@gmail.com>
2019-12-28 14:20:15 -0500

    updates bike marker with scaled marker based on avail bikes

--- src/components/BikeMarker.jsx
+++ src/components/BikeMarker.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Marker } from 'react-google-maps';
-// import bikeIcon from '../assets/images/bike.png';
+import blueMarker from '../assets/images/placeholder-svgrepo-com.svg';
 
-export const BikeMarker = ({ latitude, longitude }) => {
+export const BikeMarker = ({ latitude, longitude, size }) => {
   return (
     <Marker
       position={{ lat: latitude, lng: longitude }}
-      // icon={{
-      //   url: bikeIcon,
-      //   scaledSize: new window.google.maps.Size(24, 24)
-      // }}
+      icon={{
+        url: blueMarker,
+        scaledSize: new window.google.maps.Size(size, size)
+      }}
     />
   );
 };
