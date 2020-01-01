97263d60f04cfe665d2ae755e62bfbf6f5f59b0f
Darwin Smith <darwin911@gmail.com>
2020-01-01 18:37:46 -0500

    Renders docks available

--- src/components/Station.jsx
+++ src/components/Station.jsx
@@ -1,21 +1,21 @@
-import React, { useState } from "react";
-import moment from "moment";
+import React, { useState } from 'react';
+import moment from 'moment';
 
 const Station = ({ station }) => {
   const [details, setDetails] = useState(false);
   return (
     <article
-      className="card"
+      className='card'
       onMouseEnter={() => setDetails(!details)}
-      onMouseLeave={() => setDetails(!details)}
-    >
+      onMouseLeave={() => setDetails(!details)}>
       <h4>{station.stationName}</h4>
       <p>
-        {station.availableBikes} / {station.totalDocks} bikes{" "}
+        {station.availableBikes} / {station.totalDocks} bikes
       </p>
-      <p style={{ opacity: details ? 1 : 0, transition: "500ms" }}>
-        Updated:{" "}
-        {moment(station.lastCommunicationTime, "h:mm:ss a").format("h:mm:ss a")}
+      <p>Docks Available: {station.availableDocks}</p>
+      <p style={{ opacity: details ? 1 : 0, transition: '500ms' }}>
+        Updated:{' '}
+        {moment(station.lastCommunicationTime, 'h:mm:ss a').format('h:mm:ss a')}
       </p>
     </article>
   );
