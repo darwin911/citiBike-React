90d83b94d97e19bbe81a24a6f896e03f52023e00
Darwin Smith <darwin911@gmail.com>
2020-01-01 16:57:43 -0500

    adds links to data sourcees for future reference as comment

--- src/services/helper.js
+++ src/services/helper.js
@@ -19,3 +19,28 @@ const formatAddress = async location => {
 };
 
 export { getStationData, formatAddress };
+
+// TODO
+// http://gbfs.citibikenyc.com/gbfs/gbfs.json
+// "feeds": [
+// {
+//   "name": "station_status",
+//   "url": "https://gbfs.citibikenyc.com/gbfs/en/station_status.json"
+//   },
+//   {
+//   "name": "system_information",
+//   "url": "https://gbfs.citibikenyc.com/gbfs/en/system_information.json"
+//   },
+//   {
+//   "name": "system_alerts",
+//   "url": "https://gbfs.citibikenyc.com/gbfs/en/system_alerts.json"
+//   },
+//   {
+//   "name": "system_regions",
+//   "url": "https://gbfs.citibikenyc.com/gbfs/en/system_regions.json"
+//   },
+//   {
+//   "name": "station_information",
+//   "url": "https://gbfs.citibikenyc.com/gbfs/en/station_information.json"
+//   }
+//   ]
