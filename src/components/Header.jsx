113425b2ccc6a65ebf48ed22339b2583deacd642
Darwin Smith <darwin911@gmail.com>
2020-08-15 12:17:57 -0400

    header style updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import citiBikeLogo from '../assets/images/citibike_logo.png';
 
 export const Header = () => {
   return (
     <header>
-      <h1 className='title'>
-        Citi<span>Bike</span>
-      </h1>
+      <img src={citiBikeLogo} className='logo' alt='CitiBike Logo' />
+      {/* Citi<span>Bike</span> */}
     </header>
   );
 };
