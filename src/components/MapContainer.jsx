148a24a0455d021dd8d17e2385454501de369b41
Darwin Smith <darwin911@gmail.com>
2020-08-15 22:02:01 -0400

    updates defaultCoords/center

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -5,7 +5,7 @@ import mapStyles from '../ mapStyles';
 import { maxBy } from 'lodash';
 
 const WrappedMap = withScriptjs(
-  withGoogleMap(({ stations, resultStations }) => {
+  withGoogleMap(({ stations, resultStations, navigatorCoords }) => {
     const maxStation = maxBy(stations, (station) => station.num_docks_available);
     const maxDocks = maxStation ? maxStation.num_docks_available : 0;
 
@@ -20,11 +20,20 @@ const WrappedMap = withScriptjs(
         return <BikeMarker key={station_id} station={station} size={sizeVal} />;
       });
 
+    let defaultLatitude = 40.7359;
+    let defaultLongitude = -73.9911;
+
+    const defaultCenter = { lat: defaultLatitude, lng: defaultLongitude };
+    console.log(navigatorCoords);
     return (
       <GoogleMap
         defaultOptions={{ styles: mapStyles }}
         defaultZoom={15}
-        defaultCenter={{ lat: 40.7359, lng: -73.9911 }}>
+        defaultCenter={
+          navigatorCoords.latitude && navigatorCoords.longitude
+            ? { lat: navigatorCoords.latitude, lng: navigatorCoords.longitude }
+            : defaultCenter
+        }>
         {stationList}
       </GoogleMap>
     );
@@ -32,10 +41,13 @@ const WrappedMap = withScriptjs(
 );
 
 export const MapContainer = ({ stations, resultStations }) => {
+  const [navigatorCoords, setNavigatorCoords] = React.useState({ latitude: null, longitude: null });
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
       ({ coords }) => {
-        console.log(coords);
+        if (coords) {
+          setNavigatorCoords(coords);
+        }
       },
       (error) => console.error(error)
     );
@@ -47,11 +59,13 @@ export const MapContainer = ({ stations, resultStations }) => {
     margin: '2rem auto',
     overflow: 'hidden',
     boxShadow: '0 0 1.5rem -3px rgb(6, 43, 107)',
+    borderRadius: '0.5rem',
   };
 
   return (
     <div style={mapStyle}>
       <WrappedMap
+        navigatorCoords={navigatorCoords}
         stations={stations}
         resultStations={resultStations}
         googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_KEY}&v=3.exp&libraries=geometry,drawing,places`}
